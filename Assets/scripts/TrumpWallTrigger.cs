f87bf8b4f1db22afb21aaf902c56681453138824
dcalance <daniel.calancea@faf.utm.md>
2017-05-30 18:55:45 +0300

    Tweaked pathways

--- Assets/scripts/TrumpWallTrigger.cs
+++ Assets/scripts/TrumpWallTrigger.cs
@@ -4,36 +4,27 @@ using UnityEngine;
 
 public class TrumpWallTrigger : MonoBehaviour
 {
-    public GameObject wall1;
-    public GameObject wall2;
-
-    public GameObject enterZone1;
-    public GameObject enterZone2;
-
-    private bool disabledZone = false;
+    public GameObject wall;
 
     void OnTriggerStart(Collider other)
     {
         if (other.tag == "Hooman")
         {
-            wall1.SetActive(true);
-            wall2.SetActive(true);
+            wall.SetActive(true);
         }
     }
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Hooman")
         {
-            wall1.SetActive(true);
-            wall2.SetActive(true);
+            wall.SetActive(true);
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Hooman")
         {
-            wall1.SetActive(false);
-            wall2.SetActive(false);
+            wall.SetActive(false);
         }
     }
 }
